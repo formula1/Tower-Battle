82f29bf3ada70897e703944acfe219bf3a70eca1
formula1 <samtobia@gmail.com>
2015-11-06 19:32:39 -0800

    making it work

--- Utilities/install/box2d.js
+++ Utilities/install/box2d.js
@@ -8,20 +8,34 @@ var url = require('url');
 var dir = path.resolve(__dirname, '../..', 'web_modules');
 var nm = path.resolve(__dirname, '../..', 'node_modules');
 
-if(!fs.existsSync(dir)) fs.mkdirSync(dir);
-
 var rawHost = 'https://raw.githubusercontent.com/';
 
 var location = path.join(dir, 'Box2D_v2.3.1_debug.js');
 
-request(
-  url.resolve(rawHost, 'kripken/box2d.js/master/build/Box2D_v2.3.1_debug.js')
-).pipe(fs.createWriteStream(location));
+module.exports = new Promise(function(res, rej){
+  var counter = 0;
+  var finished = function(){
+    counter++;
+    if(counter === 2) res();
+  };
 
-location = path.join(dir, 'Box2D_v2.3.1_min.js');
+  request(
+    url.resolve(rawHost, 'kripken/box2d.js/master/build/Box2D_v2.3.1_debug.js')
+  ).pipe(fs.createWriteStream(location))
+    .on('finish', finished)
+    .on('error', rej);
 
-request(
-  url.resolve(rawHost, 'kripken/box2d.js/master/build/Box2D_v2.3.1_min.js')
-).pipe(fs.createWriteStream(location));
+  location = path.join(dir, 'Box2D_v2.3.1_min.js');
 
-fs.symlinkSync(location, path.join(nm, 'Box2D.js'));
+  request(
+    url.resolve(rawHost, 'kripken/box2d.js/master/build/Box2D_v2.3.1_min.js')
+  ).pipe(fs.createWriteStream(location))
+    .on('finish', finished)
+    .on('error', rej);
+
+}).then(function(){
+  var b2dLoc = path.join(nm, 'Box2D.js');
+  if(!fs.existsSync(b2dLoc)) fs.symlinkSync(location, b2dLoc);
+  console.log('finished with box2d');
+  return true;
+});
--- Utilities/install/index.js
+++ Utilities/install/index.js
@@ -2,13 +2,23 @@
 
 var fs = require('fs');
 var path = require('path');
+var async = require('async');
 
 var dir = path.resolve(__dirname, '../..', 'web_modules');
 if(!fs.existsSync(dir)) fs.mkdirSync(dir);
 
-fs.readdirSync(__dirname).forEach(function(file){
-  if(file === 'index.js') return;
-  require(path.join(__dirname, file));
-});
+async.each(fs.readdirSync(__dirname), function(file, next){
+  if(file === 'index.js') return next();
+  var ret = require(path.join(__dirname, file));
+  if(typeof ret === 'function') return ret(next);
+  if(ret instanceof Promise){
+    ret.catch(next);
+    ret.then(next.bind(void 0, void 0));
+    return;
+  }
 
-require('../build');
+  next();
+}, function(e){
+  if(e) throw e;
+  require('../build');
+});
